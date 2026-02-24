1d534891b6b754101c999d31e4a9f822fbb6a06b
BenoitAud <audetb2@gmail.com>
2026-02-24 03:00:01 -0500

    base for decodeur, not functional

--- src/commMemoirePartagee.c
+++ src/commMemoirePartagee.c
@@ -8,5 +8,183 @@
  ******************************************************************************/
 
 #include "commMemoirePartagee.h"
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <stdio.h>
+#include <pthread.h>
+#include <stdlib.h>
 
-// TODO: implementez ici les fonctions decrites dans commMemoirePartagee.h
+// Initialisation de la mémoire partagée (écrivain)
+int initMemoirePartageeEcrivain(const char* identifiant, struct memPartage *zone, struct videoInfos *infos)
+{
+	if (!identifiant || !zone || !infos) return -1;
+	size_t taille_header = sizeof(struct memPartageHeader);
+	size_t taille = taille_header + infos->largeur * infos->hauteur * infos->canaux;
+	printf("[initMemoirePartageeEcrivain] sizeof(memPartageHeader) = %zu\n", taille_header);
+	printf("[initMemoirePartageeEcrivain] taille totale mempartage = %zu\n", taille);
+	printf("[initMemoirePartageeEcrivain] shm_open permissions = 0666\n");
+	int fd = shm_open(identifiant, O_CREAT | O_RDWR, 0666);
+	if (fd < 0) return -1;
+	if (ftruncate(fd, taille) < 0)
+    {
+		close(fd);
+		shm_unlink(identifiant);
+		return -1;
+	}
+
+	void* ptr = mmap(NULL, taille, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+	if (ptr == MAP_FAILED)
+    {
+		close(fd);
+		shm_unlink(identifiant);
+		return -1;
+	}
+
+	struct memPartageHeader* header = (struct memPartageHeader*)ptr;
+	pthread_mutexattr_t mattr;
+	pthread_condattr_t cattr;
+	pthread_mutexattr_init(&mattr);
+	pthread_mutexattr_setpshared(&mattr, PTHREAD_PROCESS_SHARED);
+	pthread_mutexattr_setprotocol(&mattr, PTHREAD_PRIO_INHERIT);
+	pthread_condattr_init(&cattr);
+	pthread_condattr_setpshared(&cattr, PTHREAD_PROCESS_SHARED);
+	pthread_mutex_init(&header->mutex, &mattr);
+	pthread_cond_init(&header->condEcrivain, &cattr);
+	pthread_cond_init(&header->condLecteur, &cattr);
+	header->etat = ETAT_PRET_SANS_DONNEES;
+	printf("[initMemoirePartageeEcrivain] etat initial = %u (attendu: 1)\n", header->etat);
+	header->infos = *infos;
+	pthread_mutexattr_destroy(&mattr);
+	pthread_condattr_destroy(&cattr);
+	zone->fd = fd;
+	zone->header = header;
+	zone->tailleDonnees = taille - sizeof(struct memPartageHeader);
+	zone->data = (unsigned char*)ptr + sizeof(struct memPartageHeader);
+	return 0;
+}
+
+// Initialisation de la mémoire partagée (lecteur)
+int initMemoirePartageeLecteur(const char* identifiant, struct memPartage *zone) {
+	if (!identifiant || !zone) return -1;
+	int fd;
+	struct stat st;
+	void* ptr = NULL;
+	while (1)
+    {
+		fd = shm_open(identifiant, O_RDWR, 0666);
+		if (fd < 0)
+        {
+			if (errno == ENOENT)
+            {
+				usleep(DELAI_INIT_READER_USEC);
+				continue;
+			} 
+            else
+            {
+				return -1;
+			}
+		}
+
+		if (fstat(fd, &st) < 0)
+        {
+			close(fd);
+			return -1;
+		}
+
+		if ((size_t)st.st_size < sizeof(struct memPartageHeader))
+        {
+			close(fd);
+			usleep(DELAI_INIT_READER_USEC);
+			continue;
+		}
+
+		ptr = mmap(NULL, st.st_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+		if (ptr == MAP_FAILED)
+        {
+			close(fd);
+			return -1;
+		}
+
+		struct memPartageHeader* header = (struct memPartageHeader*)ptr;
+		while (header->etat == ETAT_NON_INITIALISE)
+        {
+			usleep(DELAI_INIT_READER_USEC);
+		}
+
+		zone->fd = fd;
+		zone->header = header;
+		zone->tailleDonnees = st.st_size - sizeof(struct memPartageHeader);
+		zone->data = (unsigned char*)ptr + sizeof(struct memPartageHeader);
+		return 0;
+	}
+}
+
+// Attente du lecteur (Le lecteur attend sur SA condition "condLecteur")
+int attenteLecteur(struct memPartage *zone)
+{
+	if (!zone || !zone->header) return -1;
+	pthread_mutex_lock(&zone->header->mutex);
+	while (zone->header->etat != ETAT_PRET_AVEC_DONNEES)
+    {
+        // CORRECTION : Le lecteur attend sur condLecteur (pas condEcrivain)
+		pthread_cond_wait(&zone->header->condLecteur, &zone->header->mutex);
+	}
+	return 0;
+}
+
+// Attente du lecteur Async (idem)
+int attenteLecteurAsync(struct memPartage *zone)
+{
+	if (!zone || !zone->header) return -1;
+	
+	pthread_mutex_lock(&zone->header->mutex);
+	if (zone->header->etat == ETAT_PRET_AVEC_DONNEES)
+	{
+		return 1;
+	}
+
+	pthread_mutex_unlock(&zone->header->mutex);
+	return 0;
+}
+
+// Attente de l'écrivain (L'écrivain attend sur SA condition "condEcrivain")
+int attenteEcrivain(struct memPartage *zone)
+{
+	if (!zone || !zone->header) return -1;
+	pthread_mutex_lock(&zone->header->mutex);
+
+	while (zone->header->etat != ETAT_PRET_SANS_DONNEES)
+	{
+        // CORRECTION : L'écrivain attend sur condEcrivain (pas condLecteur)
+		pthread_cond_wait(&zone->header->condEcrivain, &zone->header->mutex);
+	}
+	return 0;
+}
+
+// Signal du lecteur (Le lecteur a fini, il réveille l'écrivain sur "condEcrivain")
+void signalLecteur(struct memPartage *zone)
+{
+	if (!zone || !zone->header) return;
+
+	zone->header->etat = ETAT_PRET_SANS_DONNEES; 
+
+    // CORRECTION : On signale l'écrivain qui dort
+	pthread_cond_signal(&zone->header->condEcrivain); 
+	pthread_mutex_unlock(&zone->header->mutex);
+}
+
+// Signal de l'écrivain (L'écrivain a fini, il réveille le lecteur sur "condLecteur")
+void signalEcrivain(struct memPartage *zone)
+{
+	if (!zone || !zone->header) return;
+
+	zone->header->etat = ETAT_PRET_AVEC_DONNEES;
+
+    // CORRECTION : On signale le lecteur qui dort
+	pthread_cond_signal(&zone->header->condLecteur);
+	pthread_mutex_unlock(&zone->header->mutex);
+}
--- src/decodeur.c
+++ src/decodeur.c
@@ -1,55 +1,35 @@
 /******************************************************************************
- * Laboratoire 3
- * GIF-3004 Systèmes embarqués temps réel
- * Hiver 2026
- * Marc-André Gardner
- * 
- * Fichier implémentant le programme de décodage des fichiers ULV
+ * Laboratoire 3 - decodeur.c (Version Optimisée Pi Zero)
  ******************************************************************************/
 
-
-// Gestion des ressources et permissions
 #include <sys/resource.h>
+#include <sys/mman.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <errno.h>
 
 #include "allocateurMemoire.h"
 #include "commMemoirePartagee.h"
 #include "utils.h"
 
 #include "jpgd.h"
+#include <time.h>
 
-// Définition de diverses structures pouvant vous être utiles pour la lecture d'un fichier ULV
 #define HEADER_SIZE 4
 const char header[] = "SETR";
 
-struct videoInfos{
-        uint32_t largeur;
-        uint32_t hauteur;
-        uint32_t canaux;
-        uint32_t fps;
-};
+int main(int argc, char* argv[])
+{
+    // --- CORRECTIF 1 : Verrouiller la mémoire du processus ---
+    // Cela empêche le code et le heap d'être envoyés dans le swap,
+    // garantissant que malloc() est rapide.
+    if(mlockall(MCL_CURRENT | MCL_FUTURE) != 0) {
+        perror("[decodeur] Attention: mlockall a echoué (lancer avec sudo?)");
+    }
 
-/******************************************************************************
-* FORMAT DU FICHIER VIDEO
-* Offset     Taille     Type      Description
-* 0          4          char      Header (toujours "SETR" en ASCII)
-* 4          4          uint32    Largeur des images du vidéo
-* 8          4          uint32    Hauteur des images du vidéo
-* 12         4          uint32    Nombre de canaux dans les images
-* 16         4          uint32    Nombre d'images par seconde (FPS)
-* 20         4          uint32    Taille (en octets) de la première image -> N
-* 24         N          char      Contenu de la première image (row-first)
-* 24+N       4          uint32    Taille (en octets) de la seconde image -> N2
-* 24+N+4     N2         char      Contenu de la seconde image
-* 24+N+N2    4          uint32    Taille (en octets) de la troisième image -> N2
-* ...                             Toutes les images composant la vidéo, à la suite
-*            4          uint32    0 (indique la fin du fichier)
-******************************************************************************/
-
-
-int main(int argc, char* argv[]){
-    // On desactive le buffering pour les printf(), pour qu'il soit possible de les voir depuis votre ordinateur
-    setbuf(stdout, NULL);
-    
     // Initialise le profilage
     char signatureProfilage[128] = {0};
     char* nomProgramme = (argv[0][0] == '.') ? argv[0]+2 : argv[0];
@@ -57,13 +37,143 @@ int main(int argc, char* argv[]){
     InfosProfilage profInfos;
     initProfilage(&profInfos, signatureProfilage);
     
-    // Premier evenement de profilage : l'initialisation du programme
     evenementProfilage(&profInfos, ETAT_INITIALISATION);
     
-    // Écrivez le code de décodage depuis un fichier et d'envoi sur la zone mémoire partagée ici!
-    // N'oubliez pas que vous pouvez utiliser jpgd::decompress_jpeg_image_from_memory()
-    // pour décoder une image JPEG contenue dans un buffer!
-    // N'oubliez pas également que ce décodeur doit lire les fichiers ULV EN BOUCLE
+    if(argc < 3)
+    {
+        printf("Usage: %s [options] fichier_entree flux_sortie\n", argv[0]);
+        return -1;
+    }
 
+    const char* fichier_ulv = NULL;
+    const char* mem_sortie = NULL;
+    int argidx = 1;
+
+    if(strcmp(argv[argidx], "--debug") == 0)
+    {
+        printf("Mode debug selectionne pour le decodeur\n");
+        fichier_ulv = "240p/02_Sintel.ulv";
+        mem_sortie = "/mem1";
+    } 
+    else
+    {
+        while(argidx < argc-2 && argv[argidx][0] == '-') argidx++;
+        fichier_ulv = argv[argidx];
+        mem_sortie = argv[argidx+1];
+    }
+
+    int fd = open(fichier_ulv, O_RDONLY);
+    if(fd < 0) { perror("open ULV"); return -1; }
+
+    off_t taille_fichier = lseek(fd, 0, SEEK_END);
+    if(taille_fichier < 24) { fprintf(stderr, "Fichier ULV trop petit\n"); close(fd); return -1; }
+
+    // --- CORRECTIF 2 : MAP_SHARED au lieu de MAP_PRIVATE ---
+    // MAP_SHARED permet au kernel de libérer les pages de la vidéo immédiatement
+    // s'il a besoin de RAM pour le malloc(), car il sait qu'elles sont sur le disque.
+    // On ne met PAS MAP_POPULATE ici pour éviter de saturer la RAM au démarrage.
+    void* fichier_map = mmap(NULL, taille_fichier, PROT_READ, MAP_SHARED, fd, 0);
+    
+    if(fichier_map == MAP_FAILED) { perror("mmap ULV"); close(fd); return -1; }
+    
+    // --- CORRECTIF 3 : Optimisation de lecture ---
+    // On dit au kernel qu'on va lire séquentiellement. Il va précharger les petits bouts
+    // nécessaires (read-ahead) sans saturer la RAM.
+    madvise(fichier_map, taille_fichier, MADV_SEQUENTIAL);
+    madvise(fichier_map, taille_fichier, MADV_WILLNEED);
+
+    close(fd);
+    unsigned char* ptr = (unsigned char*)fichier_map;
+
+    if(memcmp(ptr, header, 4) != 0) {
+        fprintf(stderr, "Fichier ULV: header invalide\n");
+        munmap(fichier_map, taille_fichier);
+        return -1;
+    }
+
+    struct videoInfos infos;
+    memcpy(&infos.largeur, ptr+4, 4);
+    memcpy(&infos.hauteur, ptr+8, 4);
+    memcpy(&infos.canaux,  ptr+12, 4);
+    memcpy(&infos.fps,     ptr+16, 4);
+    size_t taille_image = infos.largeur * infos.hauteur * infos.canaux;
+
+    if(prepareMemoire(1, 1) != 0)
+    {
+        fprintf(stderr, "Erreur prepareMemoire\n");
+        munmap(fichier_map, taille_fichier);
+        return -1;
+    }
+
+    struct memPartage zone = {0};
+    if(initMemoirePartageeEcrivain(mem_sortie, &zone, &infos) != 0) {
+        fprintf(stderr, "Erreur initMemoirePartageeEcrivain\n");
+        munmap(fichier_map, taille_fichier);
+        return -1;
+    }
+
+    size_t offset = 20; 
+    int frame_count = 0;
+
+    while(1)
+    {
+        if(offset+4 > (size_t)taille_fichier) offset = 20;
+        
+        uint32_t taille_jpeg = 0;
+        memcpy(&taille_jpeg, ptr+offset, 4);
+        
+        if(taille_jpeg == 0) { offset = 20; continue; }
+        
+        if(offset+4+taille_jpeg > (size_t)taille_fichier) {
+            offset = 20;
+            continue;
+        }
+
+        evenementProfilage(&profInfos, ETAT_TRAITEMENT);
+        
+        int w = infos.largeur;
+        int h = infos.hauteur;
+        int comps = 0;
+        
+        // Tentative de décompression
+        unsigned char* img = jpgd::decompress_jpeg_image_from_memory(ptr+offset+4, taille_jpeg, &w, &h, &comps, infos.canaux);
+        
+        if(!img)
+        {
+            // Si on est ici, c'est que la RAM est vraiment pleine.
+            // Petite pause pour laisser le système respirer et espérer que kswapd libère de la place.
+            usleep(1000); 
+            fprintf(stderr, "[decodeur] OOM Frame %d. Saut.\n", frame_count);
+            offset += 4 + taille_jpeg;
+            continue;
+        }
+
+        // Sécurité dimensions
+        size_t real_size = w * h * comps;
+        if (real_size < taille_image) {
+             free(img);
+             offset += 4 + taille_jpeg;
+             continue;
+        }
+
+        evenementProfilage(&profInfos, ETAT_ATTENTE_MUTEXECRITURE);
+        attenteEcrivain(&zone);
+
+        memcpy(zone.data, img, taille_image);
+        
+        signalEcrivain(&zone);
+
+        free(img);
+
+        evenementProfilage(&profInfos, ETAT_ENPAUSE);
+
+        offset += 4 + taille_jpeg;
+        frame_count++;
+        
+        // Très important : laisse la main au kernel pour gérer la mémoire
+        sched_yield();
+    }
+
+    munmap(fichier_map, taille_fichier);
     return 0;
 }
